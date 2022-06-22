39ae076e4321ff12183893c9d2500bebcee598c1
unknown <sizwenqwala@gmail.com>
2022-06-22 14:27:09 +0200

    added containerEL

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 var state = {
     balance: 100,
-    income: 1200,
-    expense: 200,
+    income: 400,
+    expense: 100,
     transacrtion: [
                 {name: 'Salary', amount: 5000, type: income},
                 {name: 'Buy Grocery', amount: 500, type: expense},
                 {name: 'Buy Guitar', amount: 500, type: expense}
     ]
 }
- var balanceEl = document.querySelector('#balance');
- var incomeEL = document.querySelector('#income')
+ var balanceEL = document.querySelector('#balance');
+ var incomeEL = document.querySelector('#income');
+ var expenseEL = document.querySelector('#expense');
+ var transactionEL = document.querySelector('#transaction');
 
 function init() {
-    balanceEl.innerHTML = `$${state.balance}`;
+    balanceEL.innerHTML = `$${state.balance}`;
     incomeEL.innerHTML = `$${state.income}`;
+    expenseEL.innerHTML = `$${state.expense}`;
+
+    var transactionEL, containerEL, amountEL, item;
+
+    for (var i= 0; i < state.transaction.length; i++){
+                item =state.transaction[i];
+                transactionEL = document.createElement('li')  
+                transactionEL.append(state.item.name);
+                
+                transactionEL.appendChild(transactionEL);
+
+                containerEL = document.createElement('div');
+                amountEL = document.createElement('span');
+                if (item.type === 'income') {
+                  amountEL.classList.add('income-amt');
+                }else  if (item.type === 'expense') {
+                amountEL.classList.add('expense-amt');
+                }
+    }
 }
   init();
