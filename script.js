f37385e893d4a984f61cd94c8f9091d358df5dd0
unknown <sizwenqwala@gmail.com>
2022-06-22 11:04:14 +0200

    added  var balanceEl

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var state = {
     balance: 1000,
-    incoem: 1200,
+    income: 1200,
     expense: 200,
     transacrtion: [
-                {name: 'Salary', amount: 5000, type:}
+                {name: 'Salary', amount: 5000, type: income},
+                {name: 'Buy Grocery', amount: 500, type: expense},
+                {name: ' Buy Guitar', amount: 500, type: expense}
     ]
 }
+ var balanceEl = document.querySelector('#balance');
+function init() {
+balanceEl
+}
+
+init();
